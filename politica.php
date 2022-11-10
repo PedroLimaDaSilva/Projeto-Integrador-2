e00bd210f7e5d9ccd699488d745b2696cd26ed55
PedroLimaDaSilva <105168031+PedroLimaDaSilva@users.noreply.github.com>
2022-11-09 22:01:45 -0300

    Add files via upload

---
+++ politica.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title>Política de Privacidade - Geek Gate</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+    <body>
+        <?php
+            include 'nav.html';
+        ?>
+        
+        <article>
+            <h1>Política de Privacidade</h1>
+            <p></p>
+            <p></p>
+            <p></p>
+            <?php
+                include 'footer.html';
+            ?>
+        </article>
+        
+        <?php
+            include 'footer.html';
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+</html>
